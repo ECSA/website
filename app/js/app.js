d5ed3812c8db445d945bf08a56c1638f083c295d
Dylan Stankievech <dylan.stank@gmail.com>
2014-08-11 22:49:44 -0600

    Added a drop down navigation bar, and added various partial html files

--- app/js/app.js
+++ app/js/app.js
@@ -15,9 +15,17 @@ ecsaApp.config(
     $stateProvider
       .state("main", { abstract: true, url:"", templateUrl:"partials/main.html" })
       .state("main.home", { url:"/home", templateUrl:"partials/home.html"})
-      .state("main.about", { url:"/about", templateUrl:"partials/about.html"})
-      .state("main.rentals", { url:"/rentals", templateUrl:"partials/rentals.html"})
+      .state("main.about", { url:"/about", templateUrl:"partials/about/about.html"})
+      .state("main.aboutEcsa", { url:"/aboutEcsa", templateUrl:"partials/about/ecsa.html"})
+      .state("main.aboutHouse", { url:"/aboutHouse", templateUrl:"partials/about/house.html"})
+      .state("main.aboutVolunteers", { url:"/aboutVolunteers", templateUrl:"partials/about/volunteers.html"})
+      .state("main.aboutFaq", { url:"/aboutFaq", templateUrl:"partials/about/faq.html"})
+      .state("main.rentals", { url:"/rentals", templateUrl:"partials/rentals/rentals.html"})
+      .state("main.rentalItems", { url:"/rentalItems", templateUrl:"partials/rentals/items.html"})
+      .state("main.rentalHouse", { url:"/rentalHouse", templateUrl:"partials/rentals/rentHouse.html"})
       .state("main.events", { url:"/events", templateUrl:"partials/events.html"})
-      .state("main.faq", { url:"/faq", templateUrl:"partials/faq.html"})
-      .state("main.contact", { url:"/contact", templateUrl:"partials/contact.html"});
+      .state("main.contact", { url:"/contact", templateUrl:"partials/contact/contact.html"})
+      .state("main.socialMedia", { url:"/socialMedia", templateUrl:"partials/contact/socialMedia.html"})
+      .state("main.comments", { url:"/comments", templateUrl:"partials/contact/comments.html"})
+      .state("main.links", { url:"/links", templateUrl:"partials/contact/links.html"});
   });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,45 +10,25 @@ ecsaControllers.controller('contactController', function($scope, $http) {
     });
 });
 
-ecsaControllers.controller('tabController', function($rootScope, $scope, $state) {
-		$scope.go = function(route) {
-			$state.go(route);
-		};
+ecsaControllers.controller('tabController', function($rootScope, $scope, $state, $http) {
+    $http({method: 'GET', url: 'json/tabs.json', headers: {'Cache-Control': 'private, no-store, max-age=0'}}).
+        success(function(data) {
+            $scope.tabs = data;
+    });		
+    
+    $scope.go = function(route) {
+	$state.go(route);
+    };
 
-		$scope.active = function(route) {
-			return $state.is(route);
-		};
+    $scope.active = function(route) {
+	return $state.is(route);
+    };
 
-		$scope.tabs = [{
-			heading: 'Home',
-			route: 'main.home',
-			active: false
-		}, {
-			heading: 'About',
-			route: 'main.about',
-			active: false
-		}, {
-			heading: 'Rentals',
-			route: 'main.rentals',
-			active: false
-		}, {
-			heading: 'Events',
-			route: 'main.events',
-			active: false
-		}, {
-			heading: 'FAQ',
-			route: 'main.faq',
-			active: false
-		}, {
- 			heading: 'Contact Us',
-			route: 'main.contact',
-			active: false
-		}];
-
-		$scope.$on("$stateChangeSuccess", function() {
-			$scope.tabs.forEach(function(tab) {
-				tab.active = $scope.active(tab.route);
-			});
-		});
-	}
-);
+    $scope.$on("$stateChangeSuccess", function() {
+	$scope.tabs.forEach(function(tab) {
+            tab.active = $scope.active(tab.route);
+	});
+    });
+    
+    
+});
